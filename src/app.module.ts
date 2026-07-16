e3de4929c02a24c9e4cd8befb49ad38f90058624
Atri Thakar <atrithaker122@gmail.com>
2026-07-16 22:22:41 +0530

    Registered vehicle module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { PrismaService } from './prisma/prisma.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { VehicleModule } from './vehicle/vehicle.module';
 
 @Module({
-  imports: [ AuthModule, PrismaModule],
+  imports: [ AuthModule, PrismaModule, VehicleModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
