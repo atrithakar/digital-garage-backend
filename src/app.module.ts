05fab9d9644b67f1e79785a06010f494bf862c0a
Atri Thakar <atrithaker122@gmail.com>
2026-07-16 21:46:37 +0530

    removed user moudle imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { PrismaService } from './prisma/prisma.service';
 import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [UsersModule, AuthModule, PrismaModule],
+  imports: [ AuthModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
