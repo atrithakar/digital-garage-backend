66b755c6b14ea6573a2b0328034ccd003b4b891b
Atri Thakar <atri.thakar@esparkbizmail.com>
2026-07-17 15:03:00 +0530

    tag module WIP

--- src/tag/tag.controller.ts
+++ src/tag/tag.controller.ts
@@ -1,4 +1,33 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Post, Put, Delete, Body } from '@nestjs/common';
+import { TagService } from './tag.service';
+import CreateTagDto from './dto/createTagDto';
 
 @Controller('tag')
-export class TagController {}
+export class TagController {
+    constructor(private tagService: TagService) {}
+
+    @Post('create')
+    async createTag(@Body() tag: CreateTagDto) {
+        return await this.tagService.createTag(tag.name)
+    }
+
+    @Get('all')
+    async getAllTags() {
+        return await this.tagService.getAllTags()
+    }
+
+    @Get(':id')
+    async getTagById(@Param('id') id: string) {
+        return await this.tagService.getTagById(id)
+    }
+
+    @Put(':id')
+    async updateTag(@Param('id') id: string, @Body() tag: CreateTagDto) {
+        return await this.tagService.updateTag(id, tag.name)
+    }
+
+    @Delete(':id')
+    async deleteTag(@Param('id') id: string) {
+        return await this.tagService.deleteTag(id)
+    }
+}
--- src/tag/tag.service.ts
+++ src/tag/tag.service.ts
@@ -1,4 +1,46 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class TagService {}
+export class TagService {
+    constructor(private readonly prisma: PrismaService) {}
+
+    async createTag(name: string) {
+        return await this.prisma.tag.create({
+            data: {
+                name
+            }
+        })
+    }
+
+    async getAllTags() {
+        return await this.prisma.tag.findMany()
+    }
+
+    async getTagById(id: string) {
+        return await this.prisma.tag.findUnique({
+            where: {
+                id
+            }
+        })
+    }
+
+    async updateTag(id: string, name: string) {
+        return await this.prisma.tag.update({
+            where: {
+                id
+            },
+            data: {
+                name
+            }
+        })
+    }
+
+    async deleteTag(id: string) {
+        return await this.prisma.tag.delete({
+            where: {
+                id
+            }
+        })
+    }
+}
