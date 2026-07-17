154d70e81197fdf97411b6def7df256b38321e30
Atri Thakar <atri.thakar@esparkbizmail.com>
2026-07-17 18:36:56 +0530

    Added global interceptors and filters

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import dotenv from 'dotenv';
 import { ValidationPipe } from '@nestjs/common';
+import { LoggingInterceptor } from './common/interceptors/logging.interceptor';
+import { AllExceptionsFilter } from './common/filters/all-exceptions.filter';
+import { ResponseInterceptor } from './common/interceptors/response.interceptor';
 dotenv.config();
 
 async function bootstrap() {
@@ -15,6 +18,10 @@ async function bootstrap() {
       enableImplicitConversion: true,
     },
   }))
+
+  app.useGlobalInterceptors(new LoggingInterceptor())
+
+  app.useGlobalFilters(new AllExceptionsFilter())
 
   await app.listen(process.env.PORT ?? 3000);
 }
