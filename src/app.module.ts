92e2c4320ab5e77cf49777eef2c5cc03c81c893c
Atri Thakar <atri.thakar@esparkbizmail.com>
2026-07-17 13:43:24 +0530

    Added necessary imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,11 @@ import { PrismaService } from './prisma/prisma.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { VehicleModule } from './vehicle/vehicle.module';
 import { FuelLogModule } from './fuel-log/fuel-log.module';
+import { InsuranceModule } from './insurance/insurance.module';
+import { TagModule } from './tag/tag.module';
 
 @Module({
-  imports: [ AuthModule, PrismaModule, VehicleModule, FuelLogModule],
+  imports: [ AuthModule, PrismaModule, VehicleModule, FuelLogModule, InsuranceModule, TagModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
