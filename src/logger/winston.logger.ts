1c3f538c67bd43e7a2fe91d374b2c8ccc7389fe4
Atri Thakar <atri.thakar@esparkbizmail.com>
2026-07-17 18:35:00 +0530

    Added winston logger

---
+++ src/logger/winston.logger.ts
@@ -0,0 +1,22 @@
+import * as winston from 'winston';
+
+export const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.errors({ stack: true }),
+    winston.format.json(),
+  ),
+  transports: [
+    new winston.transports.Console(),
+
+    new winston.transports.File({
+      filename: 'logs/app.log',
+    }),
+
+    new winston.transports.File({
+      filename: 'logs/error.log',
+      level: 'error',
+    }),
+  ],
+});
